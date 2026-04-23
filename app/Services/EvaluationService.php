99846312263ca7d0489d2876baaf071cb481db06
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2026-04-23 15:02:12 -0300

    - EvaluationService att

--- app/Services/EvaluationService.php
+++ app/Services/EvaluationService.php
@@ -17,7 +17,11 @@ class EvaluationService{
 
     //avaliate the results (basically the final return where everything is calcd and classified)
     public function evaluate(TestResult $result){
+        $testType = $result->testType;
+        $student = $result->battery->student;
+
         //base value
+        $value = $result->v
 
         //calcs if percentile
 
@@ -33,24 +37,46 @@ class EvaluationService{
 
     //prepare data for the calcs
     public function buildMetricData(TestResult $result){
-
+        return [
+            'weight' => $result->weight ?? null,
+            //height
+            //waist
+        ];
     }
 
     //just a func to tell what type of ref it is
     public function resolveReferenceType($testType){
-
+        return $testType->calc_type === 'derived' ? 'absolute' : 'percentile';
     }
 
     public function classify(float $value, $reference){
+        return match($reference->type){
+            'percentile' => $this->classifyPercentile($value, $reference),
+            //absolute
+        };
+    }
 
+    public function calcClassPercentile($result){
+        $testTypeId = $result->test_type_id;
+        $classId = $result->battery->student->class_group_id;
+
+        $results = TestResult::whereHas('battery.student',
+            function ($q) use ($classId)){
+                $q->where('class_group_id', $classId);
+            }
     }
 
     public function classifyPercentile(float $value, $reference){
-
     }
 
     public function classifyAbsolute(float $value, $reference){
+        if($reference->min_value !== null &&
+            $reference->max_value !== null &&
+            $value >= $reference->min_value &&
+            $value <= $reference->max_value){
+                return $reference->label;
+            }
 
+        return null;
     }
-
 }
